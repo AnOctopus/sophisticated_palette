a431f70ad99b272351167e3e5b5b6f4f80863ba0
Joshua Carroll <joshua.carroll@snowflake.com>
2023-10-18 14:51:37 -0700

    cleanup

--- test_standalone.py
+++ test_standalone.py
@@ -5,7 +5,6 @@ def my_app():
     import streamlit as st
 
     st.title("My awesome app")
-
     password = st.text_input("Enter password:", key="password")
     if password and password == st.secrets.PASSWORD:
         st.write("Congrats! You can see the secret content 🎉")
@@ -22,7 +21,6 @@ def test_incorrect_password():
     assert not at.exception
 
     # Simulate a user inputting an incorrect password
-    at.text_input("password").input("bazbat")
-    at.run()
+    at.text_input("password").input("bazbat").run()
     # Should see an error since the entered password didn't match secret
     assert at.warning[0].value == "Sorry, the passwords didn't match"
